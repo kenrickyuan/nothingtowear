caa787ac5f60b95435a756a07a391366f4053871
Kenrick <kenrickyuan@gmail.com>
2022-01-03 01:48:35 +0100

    fix: Change searchSneakers fetch url

--- utils/searchSneakers.js
+++ utils/searchSneakers.js
@@ -1,5 +1,5 @@
 export const searchSneakers = async (query) => {
-  const response = await fetch(`http://localhost:3000/api/sneaker/name?query=${query}`)
+  const response = await fetch(`/api/sneaker/name?query=${query}`)
   const data = await response.json()
   return data?.products
 }
